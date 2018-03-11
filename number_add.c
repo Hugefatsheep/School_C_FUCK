54cad7448cf130759199d2fcba5cc479345e4a7d
Hugefatsheep <37077682+Hugefatsheep@users.noreply.github.com>
2018-03-11 19:59:17 +0800

    优化不能算1的问题

--- number_add.c
+++ number_add.c
@@ -8,7 +8,7 @@ int n;
 void calculate(int now, int k, int prio)
 {
 	//now记录当前长度，k记录深度，prio记录前一个的值。
-	int i = prio;
+	int i;
 	if (now > n) 
 		{ 
 		return; 
@@ -21,7 +21,7 @@ void calculate(int now, int k, int prio)
 	}
 	else
 	{
-		for (i; i > 0; i--)
+		for (i = prio; i > 0; i--)
 		{
 			if (i <= prio)  //必须必前一个要小
 			{
@@ -38,7 +38,14 @@ int main()
 {
 	while (scanf_s("%d", &n) != EOF)
 	{
-		calculate(0, 0, n - 1);
+		if (n != 1)
+		{
+			calculate(0, 0, n - 1);
+		}
+		else
+		{
+			print("1\n");
+		}
 	}
 	return 0;
 }
