2b9b9c10150486c4afc997d331847e85f1a5f21b
Hugefatsheep <37077682+Hugefatsheep@users.noreply.github.com>
2018-03-20 22:20:22 +0800

    去除重复的数
    感觉这样写0是个问题，事实上可以借助上一个去除0的函数会方便一点。（明天再改吧）

--- sort.c
+++ sort.c
@@ -16,3 +16,40 @@ int RemoveZeroElements(int socre[], int n)
 	}
 	return m;
 }
+
+int RemoveDuplicates(int socre[], int n)
+{
+	int m = 0;
+	int count_zero = 0;
+	int new_socre[100000];
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		for (int j = 1; j < n-1; j++)
+		{
+			if (socre[i] == socre[j] && socre[i] != 0)
+			{
+				socre[j] = 0;
+				m++;
+			}
+			else if (socre[i] == socre[j] && socre[i] == 0 && count_zero == 0)
+			{
+				m++;
+				count_zero++;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		if (socre[i] != 0)
+		{
+			new_socre[i] = socre[i];
+		}
+	}
+	if (count_zero != 0)
+	{
+		new_socre[i + 1] = 0;
+	}
+	return m;
+}
